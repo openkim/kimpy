66b22de2ceb00bbcaac9760e5a762476c01fc0b7
Mingjian Wen <wenxx151@umn.edu>
2017-09-04 18:00:51 -0500

    moved python padding utils to calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import numpy as np
 from kimpy import kimapi as km
 from kimpy.calculator import KIMModelCalculator
 from ase.lattice.cubic import SimpleCubic
@@ -8,8 +9,6 @@ def test_calculator():
   # create atoms object
   argon = SimpleCubic(directions=[[1,0,0], [0,1,0], [0,0,1]], size=(2,2,2),
                       symbol='Ar', pbc=(1,1,0), latticeconstant=3.0)
-  print(argon.get_cell())
-  print(argon.get_positions())
 
   # create calculator
   modelname = 'ex_model_Ar_P_MLJ_C'
@@ -18,11 +17,14 @@ def test_calculator():
   # attach calculator to atoms
   argon.set_calculator(calc)
 
+  pos = argon.get_positions()
   energy = argon.get_potential_energy()
   forces = argon.get_forces()
-  print('energy =', energy)
-  print('forces =', forces)
 
+  print('KIM model:', modelname)
+  print('\ncoords:\n', pos)
+  print('\nenergy:\n', energy)
+  print('\nforces:\n', np.reshape(forces, (-1, 3)))
 
 if __name__ == '__main__':
   test_calculator()
