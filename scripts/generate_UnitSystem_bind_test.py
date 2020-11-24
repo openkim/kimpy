b071a396a693a386e96c503bedf5bddcd05da715
Yaser Afshar <ya.afshar@gmail.com>
2020-11-24 10:33:28 -0600

    clean up the interface and remove duplicate

--- scripts/generate_UnitSystem_bind_test.py
+++ scripts/generate_UnitSystem_bind_test.py
@@ -1,13 +1,20 @@
 from generate_bind_test_commons import generate_bind, generate_test
 
-
 length_unit = {
     'FIELD_NAME': 'LENGTH_UNIT',
     'FieldName': 'LengthUnit',
     'filedName': 'lengthUnit',
     'field_name': 'length_unit',
 }
-length_attributes = ['unused', 'A', 'Bohr', 'cm', 'm', 'nm']
+
+length_attributes = [
+    'unused',
+    'A',
+    'Bohr',
+    'cm',
+    'm',
+    'nm'
+]
 
 energy_unit = {
     'FIELD_NAME': 'ENERGY_UNIT',
@@ -15,7 +22,16 @@ energy_unit = {
     'filedName': 'energyUnit',
     'field_name': 'energy_unit',
 }
-energy_attributes = ['unused', 'amu_A2_per_ps2', 'erg', 'eV', 'Hartree', 'J', 'kcal_mol']
+
+energy_attributes = [
+    'unused',
+    'amu_A2_per_ps2',
+    'erg',
+    'eV',
+    'Hartree',
+    'J',
+    'kcal_mol'
+]
 
 charge_unit = {
     'FIELD_NAME': 'CHARGE_UNIT',
@@ -23,17 +39,25 @@ charge_unit = {
     'filedName': 'chargeUnit',
     'field_name': 'charge_unit',
 }
-charge_attributes = ['unused', 'C', 'e', 'statC']
+
+charge_attributes = [
+    'unused',
+    'C',
+    'e',
+    'statC'
+]
 
 temperature_unit = {
-    'FIELD_NAME': 'TEMPERATURE_UNIT',
-    'FieldName': 'TemperatureUnit',
     'FIELD_NAME': 'TEMPERATURE_UNIT',
     'FieldName': 'TemperatureUnit',
     'filedName': 'temperatureUnit',
     'field_name': 'temperature_unit',
 }
-temperature_attributes = ['unused', 'K']
+
+temperature_attributes = [
+    'unused',
+    'K'
+]
 
 time_unit = {
     'FIELD_NAME': 'TIME_UNIT',
@@ -41,10 +65,24 @@ time_unit = {
     'filedName': 'timeUnit',
     'field_name': 'time_unit',
 }
-time_attributes = ['unused', 'fs', 'ps', 'ns', 's']
+
+time_attributes = [
+    'unused',
+    'fs',
+    'ps',
+    'ns',
+    's'
+]
 
 
-all_units = [length_unit, energy_unit, charge_unit, temperature_unit, time_unit]
+all_units = [
+    length_unit,
+    energy_unit,
+    charge_unit,
+    temperature_unit,
+    time_unit
+]
+
 all_attributes = [
     length_attributes,
     energy_attributes,
@@ -52,7 +90,6 @@ all_attributes = [
     temperature_attributes,
     time_attributes,
 ]
-
 
 for fields, attributes in zip(all_units, all_attributes):
     generate_bind(fields, attributes)
