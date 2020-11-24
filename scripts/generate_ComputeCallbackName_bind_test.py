67fb7cdea3583649fc8ef24cbfddfe6df4e1ea28
Yaser Afshar <ya.afshar@gmail.com>
2020-11-24 10:27:23 -0600

    clean up the interface

--- scripts/generate_ComputeCallbackName_bind_test.py
+++ scripts/generate_ComputeCallbackName_bind_test.py
@@ -1,6 +1,5 @@
 from generate_bind_test_commons import generate_bind, generate_test
 
-
 fields = {
     'FIELD_NAME': 'COMPUTE_CALLBACK_NAME',
     'FieldName': 'ComputeCallbackName',
@@ -8,9 +7,11 @@ fields = {
     'field_name': 'compute_callback_name',
 }
 
-
-attributes = ['GetNeighborList', 'ProcessDEDrTerm', 'ProcessD2EDr2Term']
-
+attributes = [
+    'GetNeighborList',
+    'ProcessDEDrTerm',
+    'ProcessD2EDr2Term'
+]
 
 generate_bind(fields, attributes)
 generate_test(fields, attributes)
