894b57865b3a2cb7be8b3038f98d6e69a1ef3b90
Yaser Afshar <ya.afshar@gmail.com>
2021-04-17 12:37:14 -0500

    Update the Log binding.
    Correctly use the custom destructor.
    Update the implementation, remove all the error returns and provide
    the runtime_error with expressive message in case of error.
    Add document helper to each method and update the returned values
    and input arguments.
    Correct the naming of the variables and update most of them to lower
    case letters.

--- kimpy/KIM_Log_bind.cpp
+++ kimpy/KIM_Log_bind.cpp
@@ -10,84 +10,77 @@
 namespace py = pybind11;
 using namespace KIM;
 
+
+namespace{
 struct PyLogDestroy {
   void operator()(Log *log) const {
     Log::Destroy(&log);
   }
 };
+} // namespace
+
 
 PYBIND11_MODULE(log, module)
 {
   module.doc() = "Python binding to KIM_Log.hpp";
 
-  // C++ class constructor and destructor are private, which can not be wrapped
-  // directly.
-  // So we need to call the C++ class factory function in py::init, and use
-  // `py::nodelete` to avoid calling the destructor when python instance is
-  // destroyed. It is crucial that the instance is deallocated by calling the
-  // destroy function from the C++ side to avoid memory leaks. For more info,
-  // see http://pybind11.readthedocs.io/en/stable/advanced/classes.html
-
-  py::class_<Log, std::unique_ptr<Log, PyLogDestroy> > cl(module, "Log");
+  py::class_<Log, std::unique_ptr<Log, PyLogDestroy>> cl(module, "Log");
 
   // python constructor needs to return a pointer to the C++ instance
   cl.def(py::init([]() {
-      Log * log;
+      Log *log;
       Log::Create(&log);
-      return log;
+      return std::unique_ptr<Log, PyLogDestroy>(std::move(log));
     }))
-
-
-      .def("pop_default_verbosity", &Log::PopDefaultVerbosity)
-
-      .def("get_id", &Log::GetID)
-
-      .def("set_id", &Log::SetID)
-
-      .def("push_verbosity", &Log::PushVerbosity)
-
-      .def("pop_verbosity", &Log::PopVerbosity)
-
-      // cannot simply do wrapping for the following overloaded functions as for
-      // set_parameters in KIM_Model.hpp, because here the first argument is a
-      // self-defined class. If we do that, pybind11 will report argument type
-      // matching error.
-      .def(
-          "log_entry",
-          [](Log & self,
-             LogVerbosity const logVerbosity,
-             std::string const & message,
-             int const lineNumber,
-             std::string const & fileName) {
-            self.LogEntry(logVerbosity, message, lineNumber, fileName);
-          },
-          py::arg("logVerbosity"),
-          py::arg("message"),
-          py::arg("lineNumber"),
-          py::arg("fileNmae"))
-
-      .def(
-          "log_entry",
-          [](Log & self,
-             LogVerbosity const logVerbosity,
-             std::stringstream const & message,
-             int const lineNumber,
-             std::string const & fileName) {
-            self.LogEntry(logVerbosity, message, lineNumber, fileName);
-          },
-          py::arg("logVerbosity"),
-          py::arg("message"),
-          py::arg("lineNumber"),
-          py::arg("fileNmae"));
-
+    .def("push_default_verbosity", &Log::PushDefaultVerbosity,
+         "Push a new default log_verbosity onto the KIM-API "
+         "global default verbosity stack.")
+    .def("pop_default_verbosity", &Log::PopDefaultVerbosity,
+         "Pop a log_verbosity from the KIM-API global default "
+         "verbosity stack.")
+    .def("get_id", &Log::GetID, "Get the identity of the Log object.")
+    .def("set_id", &Log::SetID, "Set the identity of the Log object.",
+         py::arg("id"))
+    .def("push_verbosity", &Log::PushVerbosity,
+         "Push a new log_verbosity onto the Log object's verbosity stack.",
+         py::arg("log_verbosity"))
+    .def("pop_verbosity", &Log::PopVerbosity,
+         "Pop a log_verbosity from the Log object's verbosity stack.")
+    // cannot simply do wrapping for the following overloaded functions as for
+    // set_parameters in KIM_Model.hpp, because here the first argument is a
+    // self-defined class. If we do that, pybind11 will report argument type
+    // matching error.
+    .def("log_entry",
+         [](Log &self,
+            LogVerbosity const &log_verbosity,
+            std::string const &message,
+            int const line_number,
+            std::string const &file_name) {
+       self.LogEntry(log_verbosity, message, line_number, file_name);
+     }, "Write a log entry into the log file.",
+        py::arg("log_verbosity"),
+        py::arg("message"),
+        py::arg("line_number"),
+        py::arg("file_name"))
+    .def("log_entry",
+         [](Log &self,
+            LogVerbosity const &log_verbosity,
+            std::stringstream const &message,
+            int const line_number,
+            std::string const &file_name) {
+      self.LogEntry(log_verbosity, message, line_number, file_name);
+    }, "Write a log entry into the log file.",
+       py::arg("log_verbosity"),
+       py::arg("message"),
+       py::arg("line_number"),
+       py::arg("file_name"));
 
   // module functions
 
   module.def("create", []() {
-    Log * log;
+    Log *log;
     Log::Create(&log);
-    return log;
-  });
-
-  module.def("destroy", [](Log * self) { Log::Destroy(&self); });
+    return std::unique_ptr<Log, PyLogDestroy>(std::move(log));
+  }, "Create a new KIM-API Log object."
+     "Return log");
 }
