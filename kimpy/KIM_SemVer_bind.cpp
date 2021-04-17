1352835a128c24c9e2e3405c42d6cd2132320c71
Yaser Afshar <ya.afshar@gmail.com>
2021-04-17 12:39:05 -0500

    Update the SEM_VER binding.
    Update the implementation, remove all the error returns and provide
    the runtime_error with expressive message in case of error.
    Add document helper to each method and update the returned values
    and input arguments.
    Correct the naming of the variables and update most of them to lower
    case letters.

--- kimpy/KIM_SemVer_bind.cpp
+++ kimpy/KIM_SemVer_bind.cpp
@@ -15,45 +15,55 @@ PYBIND11_MODULE(sem_ver, module)
   module.doc() = "Python binding to KIM_SymVer.hpp";
 
   module.def("get_sem_ver", []() {
-    std::string const & version = SEM_VER::GetSemVer();
+    std::string const &version = SEM_VER::GetSemVer();
+
     return version;
-  });
+  }, "Get the KIM-API complete Semantic Version string.",
+     "Return version");
 
-  module.def(
-      "is_less_than",
-      [](std::string const & versionA, std::string const & versionB) {
-        int isLessThan;
-        int error = SEM_VER::IsLessThan(versionA, versionB, &isLessThan);
+  module.def("is_less_than",
+      [](std::string const &version_a, std::string const &version_b) {
+    int is_less_than;
 
-        py::tuple re(2);
-        re[0] = isLessThan;
-        re[1] = error;
-        return re;
-      },
-      py::arg("versionA"),
-      py::arg("versionB"),
-      "Return(isLessThan, error)");
+    int error = SEM_VER::IsLessThan(version_a, version_b, &is_less_than);
+    if (error == 1) {
+      throw std::runtime_error(
+        "ParseSemVer (Parse Semantic Version) returns error for lhs or rhs!");
+    }
 
-  module.def(
-      "parse_sem_ver",
-      [](std::string const & version) {
-        int major;
-        int minor;
-        int patch;
-        std::string prerelease;
-        std::string buildMetadata;
-        int error = SEM_VER::ParseSemVer(
-            version, &major, &minor, &patch, &prerelease, &buildMetadata);
+    return is_less_than;
+  }, "Compare two Semantic Version strings.",
+     py::arg("version_a"),
+     py::arg("version_b"),
+     "Return is_less_than");
 
-        py::tuple re(6);
-        re[0] = major;
-        re[1] = minor;
-        re[2] = patch;
-        re[3] = prerelease;
-        re[4] = buildMetadata;
-        re[5] = error;
-        return re;
-      },
-      py::arg("version"),
-      "Return(major, minor, patch, prerelease, buildMetadata, error)");
+  module.def("parse_sem_ver", [](std::string const &version) {
+    int major;
+    int minor;
+    int patch;
+    std::string prerelease;
+    std::string build_metadata;
+
+    int error = SEM_VER::ParseSemVer(
+      version, &major, &minor, &patch, &prerelease, &build_metadata);
+    if (error == 1) {
+      throw std::runtime_error(
+        "minor and/or patch are missing!  or\n"
+        "major number has a leading zero or is not a valid integer!  or\n"
+        "minor number has a leading zero or is not a valid integer!  or\n"
+        "patch number has a leading zero or is not a valid integer!  or\n"
+        "the prerelease string is invalid!  or\n"
+        "the build metadata string is invalid!");
+    }
+
+    py::tuple re(5);
+    re[0] = major;
+    re[1] = minor;
+    re[2] = patch;
+    re[3] = prerelease;
+    re[4] = build_metadata;
+    return re;
+  }, "Parse Semantic Version string into its six components.",
+     py::arg("version"),
+     "Return(major, minor, patch, prerelease, build_metadata)");
 }
